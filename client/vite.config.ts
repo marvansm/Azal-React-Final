6aff48a23af7f57e31ee2e7e6532cd15395074bd
Marvan <marvansm@code.edu.az>
2025-12-19 15:20:34 +0400

    Fix: remove manual chunks to fix runtime bundling error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,23 +6,5 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            if (id.includes("react") || id.includes("react-dom")) {
-              return "react-vendor";
-            }
-            if (id.includes("@tanstack")) {
-              return "tanstack-vendor";
-            }
-            if (id.includes("swiper")) {
-              return "swiper-vendor";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
   },
 });
