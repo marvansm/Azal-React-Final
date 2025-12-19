839e78c0be1044e4e550026ce4d65b65b7d90eb2
Marvan <marvansm@code.edu.az>
2025-12-19 15:13:12 +0400

    Fix: vercel deployment white screen issues

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,22 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n";
 import App from "./App.tsx";
 import TanstackQueryProvider from "./Provider/TanstackQueryProvider.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TanstackQueryProvider>
-      <App />
+      <Suspense
+        fallback={
+          <div className="h-screen w-screen flex items-center justify-center bg-white text-blue-900 font-bold">
+            AZAL...
+          </div>
+        }
+      >
+        <App />
+      </Suspense>
     </TanstackQueryProvider>
   </StrictMode>
 );
